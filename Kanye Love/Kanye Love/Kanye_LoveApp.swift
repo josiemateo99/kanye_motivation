66ff56ac69610d780c854983b676329c719529c3
josiemateo99 <josiemateo99@gmail.com>
2022-05-20 12:29:47 -0400

    Inital Commit

---
+++ Kanye Love/Kanye Love/Kanye_LoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kanye_LoveApp.swift
+//  Kanye Love
+//
+//  Created by Josephine Mateo on 5/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Kanye_LoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
